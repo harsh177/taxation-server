852a8d29ac7affe2f1490b48478c9f20ff44478b
ambitious_devansh <devanshS@chainninja.com>
2018-10-11 01:25:05 +0530

    +Devansh

--- src/main/java/com/taxation/scheduling/TaxationSchedular.java
+++ src/main/java/com/taxation/scheduling/TaxationSchedular.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Component;
 
 import com.taxation.service.impl.CleaningService;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+
 @Component
 public class TaxationSchedular {
 	@Autowired
@@ -17,6 +22,38 @@ public class TaxationSchedular {
 		//System.out.println("Schedular is running"); 
 	}
 
+	@Scheduled(fixedRate = 1000 *10)
+	public void createBackUp(){
+		String executeCmd = "mysqldump -u root -p jaishreeram -B ptm -r " + "/home/npc/MyProjects/taxation-client";
+
+			System.out.println(executeCmd);
+
+			Process runtimeProcess;
+
+			try
+			{
+				runtimeProcess = Runtime.getRuntime().exec(new String[] { "cmd.exe", "/c", executeCmd });
+
+				int processComplete = runtimeProcess.waitFor();
+
+				System.out.println(processComplete);
+
+				if(processComplete == 0)
+				{
+					System.out.println("Backup Created Successfully !");
+				}
+				else
+				{
+					System.out.println("Couldn't Create the backup !");
+				}
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+
+		}
+    }
 
 
 }
